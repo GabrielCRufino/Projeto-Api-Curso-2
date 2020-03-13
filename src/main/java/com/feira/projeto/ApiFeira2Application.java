5d370f30a2735c2270f4d2df217c774cd1c151b5
Gabriel Rufino <gabriel.clara.rufino@gmail.com>
2020-03-13 11:48:46 -0300

    Esta Projeto foi criado

---
+++ src/main/java/com/feira/projeto/ApiFeira2Application.java
@@ -0,0 +1,13 @@
+package com.feira.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFeira2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFeira2Application.class, args);
+	}
+
+}
